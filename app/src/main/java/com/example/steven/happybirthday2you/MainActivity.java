7a0b97af7dce25fb38d8a7d18dd371c14c7d140e
steven-reeves <stevenreeves15@gmail.com>
2018-07-07 09:54:11 -0700

    Added date spinner and initial choices

--- app/src/main/java/com/example/steven/happybirthday2you/MainActivity.java
+++ app/src/main/java/com/example/steven/happybirthday2you/MainActivity.java
@@ -18,12 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner monthSpinner = (Spinner) findViewById(R.id.monthSpinner);
-
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.months_array, android.R.layout.simple_spinner_item);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         monthSpinner.setAdapter(adapter);
+
+
+        Spinner dateSpinner = (Spinner) findViewById(R.id.dateSpinner);
+        String[] dates = new String[]{"Enter Date Here","1","2","3","4","5","6","7","8","9","10","11","12",
+                "13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, dates);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dateSpinner.setAdapter(adapter1);
     }
 
 }
