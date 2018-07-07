73c894d6186a7d3d0b571aec8a5f661bad1d7e1d
steven-reeves <stevenreeves15@gmail.com>
2018-07-07 10:25:09 -0700

    Added verification for name entry

--- app/src/main/java/com/example/steven/happybirthday2you/MainActivity.java
+++ app/src/main/java/com/example/steven/happybirthday2you/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.steven.happybirthday2you;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.ArrayAdapter;
@@ -32,6 +35,30 @@ public class MainActivity extends AppCompatActivity {
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dateSpinner.setAdapter(adapter1);
+
+        final EditText nameText = findViewById(R.id.editText);
+
+        nameText.addTextChangedListener( new TextWatcher()
+        {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (nameText.getText().toString().length() <= 0) {
+                    nameText.setError("You must enter a name");
+                } else {
+                    nameText.setError(null);
+                }
+            }
+
+        });
+
     }
 
 }
