403b1918dd8773c45a27ecbb0226d5e79d13748d
Steven Reeves <steven@digieffects.com>
2018-07-06 23:10:12 -0700

    Added work on dropdown list

--- app/src/main/java/com/example/steven/happybirthday2you/MainActivity.java
+++ app/src/main/java/com/example/steven/happybirthday2you/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.steven.happybirthday2you;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.Adapter;
+import java.util.Arrays;
+import android.widget.ListView;
+import android.widget.AdapterView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Spinner monthSpinner1 = findViewById(R.id.monthSpinner);
+
+    String[] months_array = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, months_array);
+
+    monthSpinner1.setAdapter(adapter);
 }
