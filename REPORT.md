b537b14e7dede965acdc6a3efb3741cfcda91599
Steven Reeves <steven@digieffects.com>
2018-07-09 19:50:16 -0700

    Added form validation and member variables for input

--- REPORT.md
+++ REPORT.md
@@ -1,8 +1,46 @@
 # CST238SRS01
 Happy Birthday 2 You
 
-Name:
+Name: Steven Reeves
 
 Notes: 
 
+Create an app that allows you to enter a name, a birthday month and day (NOT the birth year).
+
+	- Name added via editText
+	- Month added via spinner
+	- Date added via spinner 
+
+Keep a count of how many people entered birthdays and display it in the app.
+
+Each entry is placed into persistent storage. If 2 dates match, the names are displayed along with the count of entries. Clear the persistent storage to reset the app.
+
+While the user enters data, validate field data and form level data.
+
+	Name field - Anything not empty
+	Month field - Any one of the 12 months, not "Select Month"
+	Date field - Any one of the dates, not "Select date"
+
+You must account for fields not being correctly entered as well as the form not being in a valid state.
+
+	- Form checked for missing fields.
+	- Form checked for invalid dates. 
+	
+
+For example, Feb 30th is not a valid date, regardless that each field is valid.
+
+	Dates confirmed invalid - Feb 30, Apr 31, Jun 31, Sep 31, Nov 31 
+
+You must allow the user to enter data in any sequence.
+
+	- Data can be added in fields in any order
+
 Issues:
+
+	- 
+
+Would be nice:
+
+	- Make Date button bigger 
+	- Error message on spinners
+	- Keyboard hangs on entering name 
--- app/src/main/java/com/example/steven/happybirthday2you/MainActivity.java
+++ app/src/main/java/com/example/steven/happybirthday2you/MainActivity.java
@@ -1,21 +1,35 @@
 package com.example.steven.happybirthday2you;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.ArrayAdapter;
 import android.widget.Adapter;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import android.widget.ListView;
 import android.widget.AdapterView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String username;
+    String usermonth;
+    String userdate;
+    String formError = "";
+    //List<String> months30 = new ArrayList<String>();
+    int usertotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         monthSpinner.setAdapter(adapter);
 
 
-        Spinner dateSpinner = (Spinner) findViewById(R.id.dateSpinner);
+        final Spinner dateSpinner = (Spinner) findViewById(R.id.dateSpinner);
         String[] dates = new String[]{"Enter Date Here","1","2","3","4","5","6","7","8","9","10","11","12",
                 "13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, dates);
@@ -56,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                     nameText.setError("You must enter a name");
                 } else {
                     nameText.setError(null);
-                    // TODO: Store name in string here.
+                    username = nameText.getText().toString();
                 }
             }
         });
@@ -68,38 +82,92 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
                 if (parentView.getItemAtPosition(position).toString().equals("Select Month") && !firstRun) {
+                    monthSpinner.requestFocus();
                     ((TextView)monthSpinner.getSelectedView()).setError("You must select a month");
                 } else {
                     firstRun = false;
-                    // TODO: Store month in string here
+                    usermonth = monthSpinner.getItemAtPosition(position).toString();
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
-                // your code here
             }
 
         });
-/*
+
         dateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             boolean firstRunDate = true;
             @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                if (parentView.getItemAtPosition(position).toString().equals("Enter Date Here") && !firstRunDate) {
-                    //((TextView)dateSpinner.getSelectedView()).setError("You must select a date");
-                    // TODO: fix this
+            public void onItemSelected(AdapterView<?> d_parentView, View d_selectedItemView, int d_Position, long d_id) {
+                if (d_parentView.getItemAtPosition(d_Position).toString().equals("Enter Date Here") && !firstRunDate) {
+                    dateSpinner.requestFocus();
+                    ((TextView)dateSpinner.getSelectedView()).setError("You must select a date");
                 } else {
                     firstRunDate = false;
-                    // TODO: Store month in string here
+                    userdate = dateSpinner.getItemAtPosition(d_Position).toString();
                 }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // your code here
+            public void onNothingSelected(AdapterView<?> d_parentView) {
             }
         });
-        */
+
+        final Button confirmBtn = findViewById(R.id.enterBtn);
+
+        confirmBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(ValidateForm())
+                {
+                    confirmBtn.setError(null);
+                    confirmBtn.setBackgroundColor(Color.GREEN);
+                }
+                else
+                {
+                    confirmBtn.getBackground().clearColorFilter();
+                    confirmBtn.requestFocus();
+                    confirmBtn.setError(formError);
+                }
+            }
+        });
+
+
+    }
+
+    private boolean ValidateForm(){
+
+        // check name, month, and date here.
+        if(username == null) {
+            formError = "Please enter a name";
+            return false;
+        }
+        if(usermonth == null || usermonth.equals("Select Month")) {
+            formError = "Please enter a month";
+            return false;
+        }
+        if(userdate == null || userdate.equals("Enter Date Here")) {
+            formError = "Please enter a date";
+            return false;
+        }
+
+        // check date and month for 30 day months
+        if(Arrays.asList("Apr", "Jun", "Sep", "Nov").contains(usermonth) &&  (Integer.parseInt(userdate) > 30))
+        {
+            formError = usermonth + " " + userdate + "is invalid.";
+            return false;
+        }
+
+        // check date and month for Feb
+        if(usermonth.equals("Feb") &&  Integer.parseInt(userdate) > 29)
+        {
+            formError = usermonth + " " + userdate + "is invalid.";
+            return false;
+        }
+
+        return true;
     }
 }
