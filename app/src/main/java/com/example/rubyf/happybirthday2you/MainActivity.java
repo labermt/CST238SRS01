aa390d6d0ff2ee3d344b9cf339a06196a6551e30
Ruby Felton <ruby.felton@oit.edu>
2018-07-10 16:17:21 -0700

    layout stacked switch to spinners

--- app/src/main/java/com/example/rubyf/happybirthday2you/MainActivity.java
+++ app/src/main/java/com/example/rubyf/happybirthday2you/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
@@ -17,8 +19,6 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 public class MainActivity extends AppCompatActivity {
 
     public EditText name;
-    public EditText month;
-    public EditText day;
     public Button add_me;
 
     public String path = Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -28,29 +28,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = (EditText) findViewById(R. id.name);
-        month = (EditText) findViewById(R. id.month);
-        day = (EditText) findViewById(R. id.day);
-
-        final FileOutputStream outputStream;
-        String fileName = "feederTxtFile";
-
-
-        Button add_me = (Button) findViewById(R. id.add_me);
-        add_me.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                try{
-                    outputStream = openFileOutput(fileName,CONTEXT.MOOD_PRIVATE);
-                    outputStream.write(String.getBytes());
-                    outputStream.close();
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-
-            }
-        });
     }
 
     public void submitBirthday(View view) {
