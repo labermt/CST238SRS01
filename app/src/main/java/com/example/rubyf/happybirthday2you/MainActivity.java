0e312f4a9fc06f6991f20a57801a2c59032bcb4e
Ruby Felton <ruby.felton@oit.edu>
2018-07-10 17:13:16 -0700

    layout stacked switch to spinners

--- app/src/main/java/com/example/rubyf/happybirthday2you/MainActivity.java
+++ app/src/main/java/com/example/rubyf/happybirthday2you/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,6 +22,11 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 public class MainActivity extends AppCompatActivity {
 
     public EditText name;
+    public Spinner month;
+    public Spinner day;
+    public TextView total;
+    public TextView person1;
+    public TextView person2;
     public Button add_me;
 
     public String path = Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -27,10 +35,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.name);
+        month = findViewById(R.id.month);
+        month.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.month_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        month.setAdapter(adapter);
+
+
+        add_me = findViewById(R.id.add_me);
 
     }
 
     public void submitBirthday(View view) {
-
+        String nameStr = name.getText().toString();
     }
 }
